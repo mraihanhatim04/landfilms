97404a2d5bf17b013a4001c194d9b2a374d38fb5
mraihanhatim <raihan04042001@gmail.com>
2024-10-13 14:50:56 +0700

    edit navbar

--- src/components/ui/MyNavbar.jsx
+++ src/components/ui/MyNavbar.jsx
@@ -17,24 +17,6 @@ const MyNavbar = () => {
             LAND FILMS
           </a>
         </NavbarBrand>
-        <NavbarContent className="text-slate-600 gap-4" justify="center">
-          <NavbarItem>
-            <a
-              href=""
-              className="text-lg font-bold tracking-wider bg-gradient-to-r from-slate-900 via-sky-700 to-red-500 bg-clip-text text-transparent"
-            >
-              In Theaters
-            </a>
-          </NavbarItem>
-          <NavbarItem>
-            <a
-              href=""
-              className="text-lg font-bold tracking-wider bg-gradient-to-r from-slate-900 via-sky-700 to-red-500 bg-clip-text text-transparent"
-            >
-              On TV
-            </a>
-          </NavbarItem>
-        </NavbarContent>
       </Navbar>
     </>
   );
